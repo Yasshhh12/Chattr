9d58f9e3d848c1cfe91ff6545da533c652162532
Yasshhh12 <prasyast152@gmail.com>
2025-01-27 02:48:34 +0530

    connected static html with server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const socketio = require('socket.io');
+
+const io = socketio(server);
+
+app.use('/',express.static(__dirname+'/frontend'));
+
+server.listen(3000,()=>{
+    console.log('Server started');
+})
