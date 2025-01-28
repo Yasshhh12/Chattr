0e7f43e93d7f330942e71d41f0378d4297be772b
Yasshhh12 <prasyast152@gmail.com>
2025-01-28 21:25:15 +0530

    rendered roomid,msg

--- index.js
+++ index.js
@@ -4,19 +4,34 @@ const app = express();
 const server = http.createServer(app);
 const socketio = require('socket.io');
 
+const connect = require('./config/database-config');
+
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    console.log('a user connected',socket.id);
+    // console.log('a user connected',socket.id);
 
-    socket.on('msg_send',(data)=>{
-        console.log(data);
-        io.emit('msg_recieved',data);
-    })
+    // socket.on('msg_send',(data)=>{
+    //     console.log(data);
+    //     io.emit('msg_recieved',data);
+    // })
+
+
 });
+
+app.set('view engine','ejs');
 
 app.use('/',express.static(__dirname+'/frontend'));
 
-server.listen(3000,()=>{
+app.get('/chat/:roomid',(req,res)=>{
+    res.render('index',{
+        name:'Yash',
+        id:req.params.roomid
+    }); 
+})
+
+server.listen(3000,async()=>{
     console.log('Server started');
+    await connect();
+    console.log("Mongodb connected");
 })
